f0bb39f4558dc1dbb51b3a4834508faa6f42d187
unknown <teddy.haiqal@gmail.com>
2025-11-02 15:12:33 +0700

    belajar tipe data dan menampilkan output

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int umurSaya = 26;
+        int minimalUmur = 30;
+
+        System.out.println(umurSaya <= minimalUmur);
+    }
+}
---
+++ tugas1.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class tugas1 {
+
+    public static void main(String[] args) {
+// Deklarasi tipe data
+
+        int StrukturBaris = 0;
+        String KataBaru = "Deklarasi tipe data String";
+        int[] empatAngka = {7, 10, 20, 23};
+        String[][] Angka = {
+            {"1", "3", "5"},
+            {"14", "19", "20"},
+            {"22", "27", "29"}
+        };
+        double angkaDesimal = 0.29;
+        LinkedList<Integer> listAngka = new LinkedList<>(List.of(22, 19, 44, 60, 72));
+
+// Menampilkan output ke terminal
+        for (String[] baris : Angka) {
+            for (String kolom : baris) {
+                System.out.print(kolom + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(StrukturBaris);
+        System.out.println(KataBaru);
+        System.out.println(Arrays.toString(empatAngka));
+        for (int angka : listAngka) {
+            System.out.print(angka + " ");
+        }
+    }
+}
